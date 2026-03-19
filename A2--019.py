174c28dd4b1a38d9e8c64df9deb22a0198e3987f
Wattikorn <07087@sukhon.ac.th>
2026-03-19 14:29:17 +0700

    asd

--- A2--019.py
+++ A2--019.py
@@ -3,18 +3,18 @@ text_up = text.upper()
 max_U = 0
 
 for i in range(len(text)) :
-    if text[i] == 'B' :
+    if text_up[i] == 'B' :
         current_U = 0
+        new_U = 0
         
         for j in range( i + 1, len(text_up)) :
             if text_up[j] == 'U':
                 current_U += 1
             elif text_up[j] == 'B':
-                new_U = 0
-                
                 for k in range( j + 1, len(text_up)) :
                     if text_up[k] == 'U':
                         new_U += 1
+                        current_U = 0
                     else:
                         break
             else:
@@ -23,7 +23,7 @@ for i in range(len(text)) :
         max_U = max(max_U, current_U, new_U)
         
 if max_U >= 2:
-    print(f'Yes {new_U}')
+    print(f'Yes {max_U}')
 elif 'B' in text_up :
     where_B = text_up.find('B')
     result = text[:where_B + 1] + ( 'U' * (len(text) - where_B - 1))
