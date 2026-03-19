583ab29f7a01587e0e679b222098e466f04e340d
Wattikorn <07087@sukhon.ac.th>
2026-03-19 14:05:47 +0700

    bitch

--- A2--019.py
+++ A2--019.py
@@ -9,13 +9,21 @@ for i in range(len(text)) :
         for j in range( i + 1, len(text_up)) :
             if text_up[j] == 'U':
                 current_U += 1
+            elif text_up[j] == 'B':
+                new_U = 0
+                
+                for k in range( j + 1, len(text_up)) :
+                    if text_up[k] == 'U':
+                        new_U += 1
+                    else:
+                        break
             else:
                 break
         
-        max_U = max(max_U, current_U)
+        max_U = max(max_U, current_U, new_U)
         
 if max_U >= 2:
-    print(f'Yes {max_U}')
+    print(f'Yes {new_U}')
 elif 'B' in text_up :
     where_B = text_up.find('B')
     result = text[:where_B + 1] + ( 'U' * (len(text) - where_B - 1))
